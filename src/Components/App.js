7ba424c33f05ee2f87612c34f34df271f292d7eb
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-11 10:46:39 +0300

    Working

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@
 import React,{useState,useEffect} from 'react';
 import MovieBox from './MovieBox';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar,nav } from 'react-bootstrap';
 
 
 const api ="https://api.themoviedb.org/3/movie/popular?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&language=en-US&page=1"
@@ -19,18 +20,48 @@ function App() {
     },[])
     
   return (
+    <>
+
+    
     <div className='container'>
         <div className='grid'>
         {movies.map(singleMovie=>
           
         <MovieBox  key={singleMovie.id} {...singleMovie}/>
         )
-   
+    
     }
     </div>
     </div>
+    
+    
+    
+    </>
+    
   )
 }
 
-export default App
+export default App;
+
+
+/*
+<Navbar bg='dark'   expand='1g' variant='dark'>
+      <Container   fluid>
+       <Navbar.Brand href=''>MovieDB App</Navbar.Brand>
+       <Navbar.Brand href=''>Trending</Navbar.Brand>
+       <Navbar.Toggle aria-controls='navbarScroll'></Navbar.Toggle>
+
+       <Navbar.Collapse id='navbarScroll'>
+        <nav className='me-auto my-2 my-1g-3'>
+          style={{maxHeight:'100px'}}
+          navbarScroll>
+        </nav>
+       </Navbar.Collapse>
+      </Container>
+
+
+
+
+    </Navbar>
+    */
 
